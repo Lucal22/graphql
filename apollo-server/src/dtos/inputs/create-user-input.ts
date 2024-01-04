523d9e3a34d4b2315b54cdeaad6fb84f7f855933
lucaldev <lucaldev22@gmail.com>
2024-01-04 19:11:21 -0300

    feat: Adding validation to input.

--- apollo-server/src/dtos/inputs/create-user-input.ts
+++ apollo-server/src/dtos/inputs/create-user-input.ts
@@ -1,11 +1,17 @@
+import { IsString } from 'class-validator';
 import { Field, InputType } from 'type-graphql';
 
 @InputType()
 export class CreateUserInput {
+  @IsString()
   @Field()
   customerId: string;
+
+  @IsString()
   @Field()
   name: string;
+
+  @IsString()
   @Field()
   lastName: string;
 }
